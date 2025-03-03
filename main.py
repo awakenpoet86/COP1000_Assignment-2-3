329e4b1a6e63881f66de93918e77d8d6e431d4e0
awakenpoet86 <awakenpoet86@gmail.com>
2025-03-02 22:56:33 -0500

    Furniture.py

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ retailPrice = 325.00
 wholesalePrice = 200.00
 
 # Write your assignment statements here for profit, salePrice, and saleProfit
-
+profit = retailPrice-wholesalePrice 
+salesPrice = retailPrice * 0.75
+saleProfit = salePrice - wholesalePrice
 print("Item Name: " + itemName)
 print("Retail Price: $" + str(retailPrice))
 print("Wholesale Price: $" + str(wholesalePrice))
